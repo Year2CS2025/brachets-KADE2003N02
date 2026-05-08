2082632989a1f1ebf66fbe3aa9a1a66278bbc8b8
KWIZERA David <kwizeradev1@gmail.com>
2026-05-08 10:15:40 +0200

    This is the Bracketing assignment where we checked the array of the symbols in an array and see/ prove if it is well balanced  or not

--- Balanced.java
+++ Balanced.java
@@ -7,9 +7,26 @@ public class Balanced{
         
     }
     public static boolean isBalanced(String s){
-        //
+        Stack<Character> stack = new Stack<>();
         
-        return false;
+        for(char c : s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }
+            else {
+                // It's a closing bracket
+                if(stack.isEmpty()) return false;
+                
+                char open = stack.pop();
+                if((c == ')' && open != '(') ||
+                   (c == '}' && open != '{') ||
+                   (c == ']' && open != '[')){
+                    return false;
+                }
+            }
+        }
+        
+        return stack.isEmpty();
 }
         
     
